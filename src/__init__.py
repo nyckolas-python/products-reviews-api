7ceb1b7e411275ebc4fa504624a74db115bcd0b1
nyckolas <nyckolas.work@gmail.com>
2022-11-04 22:01:04 +0200

    Add sql_debug

--- src/__init__.py
+++ src/__init__.py
@@ -3,7 +3,7 @@ import config
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, get_debug_queries
 
 
 app = Flask(__name__)
@@ -12,5 +12,22 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 api = Api(app)
 
+app.debug = True
+
+
+def sql_debug(response):
+    queries = list(get_debug_queries())
+    total_duration = 0.0
+    for q in queries:
+        total_duration += q.duration
+
+    print('=' * 80)
+    print(' SQL Queries - {0} Queries Executed in {1}ms'.format(len(queries), round(total_duration * 1000, 2)))
+    print('=' * 80)
+
+    return response
+
+
+app.after_request(sql_debug)
 
 from src import routes, models
