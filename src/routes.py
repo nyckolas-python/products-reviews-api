43993eeb69c0a5f0c138e12f93f18b087b4b20db
nyckolas <nyckolas.work@gmail.com>
2022-11-07 12:15:00 +0200

    Fix routes

--- src/routes.py
+++ src/routes.py
@@ -2,4 +2,6 @@ from src import api
 from src.resourses import ProductApi
 
 
-api.add_resource(ProductApi, '/api/v1/products/<int:id>', strict_slashes=False)
+# api.add_resource(ProductApi, '/api/v1/products/', strict_slashes=False)
+# api.add_resource(ProductApi, '/api/v1/products/<int:id>', strict_slashes=False)
+api.add_resource(ProductApi, '/api/v1/products/<int:id>/reviews', strict_slashes=False)
